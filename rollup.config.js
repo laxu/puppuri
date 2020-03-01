4ac3b74a94cec89f915946f71394712f6f7bdd9c
laxu <hopealusikka@gmail.com>
2020-03-01 13:09:52 +0200

    feat: Add script minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import json from '@rollup/plugin-json';
+import {terser} from 'rollup-plugin-terser';
 
 module.exports = {
-  input: 'index.js',
-  plugins: [json()],
-  output: {
-  	name: 'puppuri',
-    file: 'bundle.js',
-    format: 'iife'
-  }
+	input: 'index.js',
+	plugins: [json()],
+	output: [
+		{
+			name: 'puppuri',
+			file: 'bundle.js',
+			format: 'iife',
+		},
+		{
+			file: 'bundle.min.js',
+			format: 'iife',
+			name: 'version',
+			plugins: [terser()]
+		}
+		
+	]
 };
